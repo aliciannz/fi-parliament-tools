db9d491b07981e7a00725f39c59b94b366b60fa0
Alicia <26570278+aliciannz@users.noreply.github.com>
2022-05-23 19:02:05 +0300

    Refactor speaker extraction and video utils code
    * Only obtain the frames of each scene temporarily instead of all video's frames
    * Use the distances between faces in consecutive frames to determine the trajectories of faces instead of the speaker's ids
    * Small fixes to the speaker extraction code

--- src/fi_parliament_tools/speaker_extraction.py
+++ src/fi_parliament_tools/speaker_extraction.py
@@ -12,41 +12,27 @@ from alive_progress import alive_bar
 from syncnet.run_syncnet import run_syncnet
 from syncnet.run_pipeline import run_pipeline
 from argparse import Namespace
-import multiprocessing
-import os
 import shutil
-import glob
 from fi_parliament_tools.video_utils.create_videos import VideoCreatorPipeline
-import time
 
-
+ 
 class SpeakerExtractionPipeline(Pipeline):
-    """
-    TODO: Comments
-    """
     def __init__(
-        self, metadata_path: Path, video_path: Path, output_path: Path
+        self, metadata_path: str, video_path: str, output_path: str
     ) -> None:
-        # super().__init__(log)
-        # self.log = log
         self.metadata_path = Path(metadata_path).resolve()
         self.video_path = Path(video_path).resolve()
         self.output_path = output_path
         self.output_path.mkdir(exist_ok=True)
 
-        self.threshold = 200  # TODO: Establish a threshold
+        self.distance_threshold = 200  
+        self.missing_threshold = 150
+        self.cost_threshold = 30
         self.frames_per_clip = 200
 
         self.width_height = [1920, 1080]
         self.center_of_vid = [elem / 2 for elem in self.width_height]
-
         self.video_name = self.obtain_session_name(video_path)
-
-        frames_path = os.path.join(f"data/processed/{self.video_name}/frames", "*.jpg")
-        self.flist = glob.glob(frames_path)
-        self.flist.sort()
-
-        self.video_pipeline = VideoCreatorPipeline(frames_path, video_path)
 
     def load_metadata(self, path: Path):
         with path.open(mode="r", encoding="utf-8", newline="") as file:
@@ -62,19 +48,46 @@ class SpeakerExtractionPipeline(Pipeline):
         video_name = video_name.split("-", 1)[1]
         return video_name
 
+    def obtain_center_bb(self, coords): 
+        center_point = [coords[0] / 2 + coords[2], coords[1] / 2 + coords[3]]
+        return center_point
+
     def find_centered_speakers(self, frame_data: List[Dict[str, Any]]):
         frame_coords = [speaker_info["coords"] for speaker_info in frame_data]
-        converted_coords = [
-            (coord[0] / 2 + coord[2], coord[1] / 2 + coord[3]) for coord in frame_coords
-        ]
-        diff = np.array(converted_coords) - np.array(self.center_of_vid)
+        conv_coords = [self.obtain_center_bb(coord) for coord in frame_coords]
+
+        diff = np.array(conv_coords) - np.array(self.center_of_vid)
         distances = np.linalg.norm(diff, axis=1)  # type: ignore
         indices = np.argsort(distances)
-        center_faces_inds = np.where(
-            distances <= self.threshold + distances[indices[:1]]
-        )[0]
-        speakers = [frame_data[index] for index in center_faces_inds]
+        condition = (distances <= self.distance_threshold + distances[indices[:1]])
+        centered_inds = np.where(condition)[0]
+        speakers = [frame_data[index] for index in centered_inds]
         return speakers
+
+    def compute_distances(self, speakers, faces):
+        faces_coords = np.array([self.obtain_center_bb(data[-1][1]) for data in faces.values()])
+        speaker_coords = np.array([self.obtain_center_bb(speaker['coords']) for speaker in speakers])
+        distances = np.zeros((len(speaker_coords), len(faces_coords)))
+        if len(faces_coords)!=0: # In other words, if its not the first frame
+            for i, speaker in enumerate(speaker_coords):
+                for j, face in enumerate(faces_coords):
+                    distances[i][j] = np.linalg.norm(face - speaker)
+        return distances 
+
+    def determine_speaker_position(self, distances, speakers):
+        # https://stackoverflow.com/questions/31694080/get-an-index-of-a-sorted-matrix
+        sorted_min_values = list(zip(*np.argsort(distances, axis=None).__divmod__(distances.shape[1])))
+        positions = [-1] * len(speakers)
+        speakers_used = []
+        indices_used = []
+        for speaker, index in sorted_min_values:
+            cost = distances[speaker][index]
+            if speaker not in speakers_used and index not in indices_used:
+                    if cost <= self.cost_threshold: 
+                        positions[speaker] = index
+                        indices_used.append(index)
+                        speakers_used.append(speaker)
+        return positions
 
     def detect_faces_in_scene(
         self,
@@ -82,47 +95,32 @@ class SpeakerExtractionPipeline(Pipeline):
         start_scene: int,
         end_scene: int,
     ) -> None:
-        faces = defaultdict(list)
-        last_frame = start_scene
-        speaker_cuts = []
-        last_speaker_used = {}
+        saved_faces = defaultdict(list)
         for frame in range(start_scene, end_scene):
-            if frame in frames.keys() and frame == last_frame + 1:
+            if frame in frames.keys():
                 frame_data = frames[frame]
                 speakers = self.find_centered_speakers(frame_data)
-                for speaker in speakers:
+                distances = self.compute_distances(speakers, saved_faces)
+                positions = self.determine_speaker_position(distances, speakers)
+                for j, speaker in enumerate(speakers):
                     name = speaker["speaker_id"]
                     frame_coords = (frame, speaker["coords"])
-                    if name in faces.keys():
-                        if speaker_cuts.count(name) == 0:
-                            name_to_use = name
-                            last_frame_speaker = faces[name][-1]
-                        else:
-                            name_to_use = last_speaker_used[name]
-                            last_frame_speaker = faces[name_to_use][-1]
-
-                        if last_frame_speaker[0] == frame - 1:
-                            faces[name_to_use].append(frame_coords)
-                            last_speaker_used[name] = name_to_use
-                        else:
-                            speaker_cuts.append(name)
-                            new_name = f"{name}_{speaker_cuts.count(name)}"
-                            faces[new_name].append(frame_coords)
-                            last_speaker_used[name] = new_name
+                    if positions[j]==-1:
+                        name += '_x' if name in saved_faces.keys() else ''
                     else:
-                        faces[name].append(frame_coords)
-            last_frame = frame
-        return faces
+                        name = list(saved_faces.keys())[positions[j]]
+                    saved_faces[name].append(frame_coords)
+        return saved_faces
 
-    def filter_short_videos(self, faces):
+    def filter_short_videos(self, faces): 
         new_faces = defaultdict(list)
         for face in faces.items():
             speaker, coords_list = face
-            if len(coords_list) > self.frames_per_clip:
+            if len(coords_list) >= self.frames_per_clip:
                 new_faces[speaker] = coords_list
         return new_faces
 
-    def detect_lip_activity(self, video):
+    def run_syncnet(self, video):
         model_path = Path("models", "syncnet_v2.model")
         ref_name = video.split("/")[-2:]
         ref_name = "/".join(ref_name)[:-4]
@@ -140,22 +138,7 @@ class SpeakerExtractionPipeline(Pipeline):
         shutil.rmtree(tmp_path)
         return float(conf)
 
-    def save_lip_activity(self, videos, n_scene, speaker_frames, speaker_coords):
-        aux = []
-        for i, video in enumerate(videos):
-            conf = self.detect_lip_activity(video)
-            if conf > 1:
-                xx = {
-                    "conf": conf,
-                    "start_frame": speaker_frames[i][0],
-                    "end_frame": speaker_frames[i][1],
-                    "scene": n_scene,
-                    "coords": speaker_coords[i],
-                }
-                aux.append(xx)
-        return aux
-
-    def divide_speeches(self, coords):
+    def divide_speeches_in_clips(self, coords): 
         last_frame, first_frame = int(coords[-1][0]), int(coords[0][0])
         total_frames = last_frame - first_frame
         n_clips, remainder = divmod(total_frames, self.frames_per_clip)
@@ -173,47 +156,35 @@ class SpeakerExtractionPipeline(Pipeline):
             coords_to_use = coords[coords_length : coords_length + length_frames]
             coords_to_use = [coords[1] for coords in coords_to_use]
             coords_length += length_frames
-
             coords_of_speaker.append(coords_to_use)
+
         return frames_of_speaker, coords_of_speaker
 
-    def save_conf_to_json(self, data, scene_path):
+    def detect_lip_activity(self, videos, n_scene, speaker_frames, speaker_coords):
+        lip_activity = []
+        confidences = [self.run_syncnet(video) for video in videos]
+        if len(confidences)!=0:
+            count  = sum(map(lambda x : x>1, confidences))
+            if (count/len(confidences))*100 >= 80:
+                confidences = [2 if conf <= 1 else conf for conf in confidences]
+            for i, conf in enumerate(confidences):
+                if conf > 1:
+                    speaker_data = {
+                        "conf": conf,
+                        "start_frame": speaker_frames[i][0],
+                        "end_frame": speaker_frames[i][1],
+                        "scene": n_scene,
+                        "coords": speaker_coords[i],
+                    }
+                    lip_activity.append(speaker_data)
+        return lip_activity
+
+
+    def save_confs_to_json(self, data, scene_path):
         file_path = Path(scene_path, "lip_activity.json")
         if len(data) != 0:
             with open(file_path, "w") as outfile:
                 json.dump(data, outfile)
-
-    def run(self) -> None:
-        frames, scenes = self.load_metadata(self.metadata_path)
-        path = Path(self.output_path, self.video_name)
-        path.mkdir(exist_ok=True)
-
-        with alive_bar(int(len(scenes) / 2)) as bar:
-            for start_scene, end_scene in zip(scenes, scenes[1:]):
-                scene_path = Path(path, f"{start_scene}-{end_scene}")
-                scene_path.mkdir(exist_ok=True)
-                faces = self.detect_faces_in_scene(frames, start_scene, end_scene)
-                faces = self.filter_short_videos(faces)  # !! Fix this.
-                speaker_confs = defaultdict(list)
-
-                for speaker, coords_list in faces.items():
-                    speaker_frames, speaker_coords = self.divide_speeches(coords_list)
-                    videos = []
-                    for j in range(len(speaker_frames)):
-                        video = self.video_pipeline.generate_audio_video(
-                            scene_path, speaker, speaker_frames[j], speaker_coords[j]
-                        )
-                        videos.append(video)
-                    print("Detecting lip activity")
-                    n_scene = f"{start_scene}-{end_scene}"
-                    lip_activity = self.save_lip_activity(
-                        videos, n_scene, speaker_frames, speaker_coords
-                    )
-
-                    if len(lip_activity) != 0:
-                        speaker_confs[speaker].extend(lip_activity)
-                self.save_conf_to_json(speaker_confs, scene_path)#
-            bar()
 
     def read_jsons(self):
         json_paths = Path("data/processed/033-2020").glob("**/lip_activity.json")
@@ -237,12 +208,44 @@ class SpeakerExtractionPipeline(Pipeline):
         with open(file_path, "w") as outfile:
             json.dump(speeches, outfile)
 
+    def run(self) -> None:
+        frames, scenes = self.load_metadata(self.metadata_path)
+        path = Path(self.output_path, self.video_name)
+        path.mkdir(exist_ok=True)
+        with alive_bar(len(scenes)) as bar:
+            for start_scene, end_scene in zip(scenes, scenes[1:]):
+                print(f"Scene: {start_scene}-{end_scene}")
+                scene_path = Path(path, f"{start_scene}-{end_scene}")
+                scene_path.mkdir(exist_ok=True)
+                faces = self.detect_faces_in_scene(frames, start_scene, end_scene)
+                faces = self.filter_short_videos(faces) # Filter captured speakers that are less than 200 frames overall
+                
+                if faces:
+                    speaker_confs = defaultdict(list)
+                    self.video_pipeline = VideoCreatorPipeline(path, self.video_path, self.video_name, start_scene, end_scene)
+                    self.video_pipeline.obtain_frames()
+
+                    for speaker, coords_list in faces.items():
+                        speaker_frames, speaker_coords = self.divide_speeches_in_clips(coords_list)
+                        videos = []
+                        for j in range(len(speaker_frames)):                       
+                            video = self.video_pipeline.generate_video(speaker, speaker_frames[j], speaker_coords[j])
+                            videos.append(video)
+                        n_scene = f"{start_scene}-{end_scene}"
+                        lip_activity = self.detect_lip_activity(videos, n_scene, speaker_frames, speaker_coords)  
+                        if len(lip_activity) != 0:
+                            speaker_confs[speaker].extend(lip_activity)
+
+                    self.video_pipeline.clean_temporal_files()
+                    self.save_confs_to_json(speaker_confs, scene_path)
+                bar()
+
 
 output_path = Path("data/processed")
 metadata_path = Path("data/processed/033-2020/033-2020_faces.json")
 video_path = Path("data/raw/033-2020/session-033-2020.mp4")
+
 test = SpeakerExtractionPipeline(
     metadata_path=metadata_path, video_path=video_path, output_path=output_path
 )
 test.run()
-#test.read_jsons()
--- src/fi_parliament_tools/video_utils/create_videos.py
+++ src/fi_parliament_tools/video_utils/create_videos.py
@@ -4,22 +4,26 @@ from fi_parliament_tools.pipeline import Pipeline
 from pathlib import Path
 import glob
 import os
-
+import shutil
 class VideoCreatorPipeline(Pipeline):
-    def __init__(self, frames_path: Path, video_path: Path) -> None:
-        # self.video_name = "033-2020"
-        self.flist = glob.glob(frames_path)
-        self.flist.sort()
+    def __init__(self, output_path: Path, video_path: Path, video_name: str, start_scene: int, end_scene: int) -> None:
+        self.output_path = output_path 
         self.video_path = video_path
+        self.video_name = video_name
+        
+        self.start_scene = start_scene
+        self.end_scene = end_scene
+        self.scene_path = Path(self.output_path, f"{start_scene}-{end_scene}")
+        self.frames_path = Path(self.output_path, self.video_name)
         self.size = 224 #!!
         self.fps = 25  #!!
 
     def add_padding(self, face):
         width = max(self.size, face[0])
         height = max(self.size, face[1])
-        x = face[2] - 60 #!!
-        y = face[3] - 60 #!!
-        if width <= 0 or height <= 0:
+        x = max(face[2] - 60, 0) #!!
+        y = max(face[3] - 60, 0) #!!
+        if (width <= 0 or height <= 0):
             raise ValueError("Face crop width or height is less than 0.")
         face_coords = [width, height, x, y]
         return face_coords
@@ -61,27 +65,52 @@ class VideoCreatorPipeline(Pipeline):
             print(msg)
         return output_video
 
-    def generate_audio_video(self, total_path, speaker, frames_speaker, coords_speaker):
+
+    def obtain_frames(self):
+        frames_path = Path(self.output_path, f"frames_{self.start_scene}-{self.end_scene}")
+        frames_path.mkdir(exist_ok=True)
+        video_start = self.convert_to_timestamp(self.start_scene)
+        video_end = self.convert_to_timestamp(self.end_scene)
+        command = f"ffmpeg -y -i {self.video_path} -ss {video_start} -to {video_end} -qscale:v 2 -f image2 {Path(frames_path, '%06d.jpg')}"
+        try:
+            subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        except subprocess.CalledProcessError as e:
+            msg = f"ffmpeg returned non-zero exit status {e.returncode}. Stderr:\n {e.stderr}"
+            print(msg)
+        self.frames_path = frames_path
+        return frames_path
+
+    def generate_video(self, speaker, frames_speaker, coords_speaker):
         codec = cv2.VideoWriter_fourcc(*"XVID")
         start_frame, end_frame = frames_speaker
 
-        name_video = Path(total_path, f"{speaker}_{start_frame}_{end_frame}")
+        name_video = Path(self.scene_path, f"{speaker}_{start_frame}_{end_frame}")
         tmp_video = Path(str(name_video) + "_tmp.avi")
-
         video_writer = cv2.VideoWriter(
             str(tmp_video), codec, self.fps, (self.size, self.size)
         )
+        frames_images = os.path.join(self.frames_path, "*.jpg")
+        flist = glob.glob(frames_images)
+        flist.sort()
+
         for index, n_frame in enumerate(range(start_frame, end_frame)):
             coords = self.add_padding(coords_speaker[index])
             w, h, x, y = coords
-            image = cv2.imread(self.flist[n_frame])
+            index_n = n_frame - self.start_scene
+            image = cv2.imread(flist[index_n])
             frame = image[y : y + h, x : x + w]
+            
             video_writer.write(frame)
+            
         video_writer.release()
-        audio_file = self.generate_audio(total_path, start_frame, end_frame)
-        
+        audio_file = self.generate_audio(self.scene_path, start_frame, end_frame)
         video_file = self.combine_audio_video(audio_file, name_video)
+
         # We only need the video file
         audio_file.unlink()
         tmp_video.unlink()
+
         return video_file
+
+    def clean_temporal_files(self):
+        shutil.rmtree(self.frames_path)
